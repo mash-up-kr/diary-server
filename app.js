967a4d185a45d43810ac9dc20ace1c02819bbe4b
pilju - Lee <di002@naver.com>
2017-03-20 14:45:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,30 +5,22 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-// var routes = require('./routes/index');
 const account = require('./routes/account');
 const diary = require('./routes/diary');
 const keyword = require('./routes/keyword');
 
 const app = express();
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes);
 app.use('/account', account);
 app.use('/diary', diary);
 app.use('/keyword', keyword);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   let err = new Error('Not Found');
   err.status = 404;
@@ -48,16 +40,6 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-    res.send({
-      status: 'error',
-      result: err.message
-    });
-});
 
 
 module.exports = app;
